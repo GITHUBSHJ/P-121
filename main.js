3afc94dfa11f8c9ac1a7671844494df5ea63bc73
SWASTIK SIBAM <89509745+GITHUBSHJ@users.noreply.github.com>
2022-04-29 12:52:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log(
+	"%cAccording to my case study google lens is more accurate than mobileNet.",
+	"color: white; background:linear-gradient(#E66465, #9198E5); padding: 1.2em; border-radius:5px;"
+);
